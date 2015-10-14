b34f9f19cce5e7cbcb25fbb4b30a32d6f12765b0
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-10-14 20:59:07 +1100

    Starting to parse command line options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate getopts;
+
+use getopts::Options;
+use std::env;
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} URL [options]", program);
+
+    println!("{}", opts.usage(&brief));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    let mut options = Options::new();
+
+    println!("{:?}", args);
 }
