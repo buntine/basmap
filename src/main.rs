d724321e832162fc7131894c8e40f2bd2fb3ce17
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-10-14 21:16:32 +1100

    Set basic options

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,21 @@ fn print_usage(program: &str, opts: Options) {
     println!("{}", opts.usage(&brief));
 }
 
+fn build_options() -> Options {
+    let mut options = Options::new();
+
+    options.optopt("c", "concurrency", "Sets number of concurrent HTTP requests (default 3)", "NUMBER");
+    options.optopt("s", "sleep", "Sets number of milliseconds to sleep between requests (default 1000)", "NUMBER");
+    options.optflag("h", "help", "Print this help menu");
+    options.optflag("r", "redirects", "Consider HTTP redirects (30x) successfuly");
+    options.optflag("v", "verbose", "Print progress verbosely");
+
+    options
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
-    let mut options = Options::new();
+    let mut options = build_options();
 
-    println!("{:?}", args);
 }
