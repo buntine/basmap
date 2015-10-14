b4743e79fe4472a428481d8a7a106be79b029ecd
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-10-14 21:32:26 +1100

    Printing help message

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,8 @@ fn print_usage(program: &str, opts: Options) {
 fn build_options() -> Options {
     let mut options = Options::new();
 
-    options.optopt("c", "concurrency", "Sets number of concurrent HTTP requests (default 3)", "NUMBER");
-    options.optopt("s", "sleep", "Sets number of milliseconds to sleep between requests (default 1000)", "NUMBER");
+    options.optopt("c", "", "Amount of concurrent HTTP requests (default 3)", "NUMBER");
+    options.optopt("s", "", "Milliseconds to sleep between requests (default 1000)", "NUMBER");
     options.optflag("h", "help", "Print this help menu");
     options.optflag("r", "redirects", "Consider HTTP redirects (30x) successfuly");
     options.optflag("v", "verbose", "Print progress verbosely");
@@ -24,6 +24,14 @@ fn build_options() -> Options {
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
-    let mut options = build_options();
+    let options = build_options();
+    let matches = match options.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(e) => { panic!(e.to_string()) }
+    };
 
+    if (matches.opt_present("h")) {
+        print_usage(&program, options);
+        return;
+    }
 }
